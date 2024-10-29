6cfb3f684c893bf2b1a9dfcbbe05c23540478125
vitorDani3l <votoro.vd@gmail.com>
2024-10-29 10:28:23 -0400

    Create calculadora.sh
    Create file Calculadora em sh

---
+++ calculadora.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Tornando o arquivo executável
+
+chmod 744 calculadora.sh
+
+#Conhecendo o usuário
+
+echo "Digite seu nome: "
+read nome
+echo "Seja bem vindo $nome, Vamos calcular!!"
+
+
+#Calculadora em python
+
+python3 /home/vitor/modulo1/projeto/calculadora.py
+
